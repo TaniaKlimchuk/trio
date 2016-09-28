2847809c497111e6bf781ebeddee388fecfd1e0c
Tania <tania-klimchuk94@mail.ru>
2016-09-28 15:09:38 +0300

    merge

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <QApplication>
+#include <QQmlApplicationEngine>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+int an = 50;
+    return app.exec();
+}
